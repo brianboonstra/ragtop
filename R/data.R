c3a95a52241db4408256808b30333138917ff889
Maximilian Muecke <muecke.maximilian@gmail.com>
2025-07-16 21:35:39 +0200

    docs: modernise data roxygen2

--- R/data.R
+++ R/data.R
@@ -1,16 +1,14 @@
 #' Market information snapshot for TSLA options
 #'
-#' A dataset containing option contract details and a
-#'  snapshot of market prices
-#'  for Tesla Motors (TSLA) equity options, interest rates and an equity price.
+#' A dataset containing option contract details and a snapshot of market prices
+#' for Tesla Motors (TSLA) equity options, interest rates and an equity price.
 #'
-#' The TSLAMarket list contains three elements:
+#' @format A list with these prices and rates:
 #' \describe{
-#' \item{\code{S0}}{The stock price as of snapshot time}
-#' \item{\code{risk_free_rates}}{The spot risk-free rate curve as of snapshot time}
-#' \item{\code{options}}{A data frame with details of the options market}
+#'   \item{\code{S0}}{The stock price as of snapshot time}
+#'   \item{\code{risk_free_rates}}{The spot risk-free rate curve as of snapshot time}
+#'   \item{\code{options}}{A data frame with details of the options market}
 #' }
-#' @docType data
-#' @usage data(TSLAMarket)
-#' @format A list with these prices and rates
+#' @examples
+#' TSLAMarket
 "TSLAMarket"
